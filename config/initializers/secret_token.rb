15de3773bf8fd9d5ef5e458776117cbceeb4a4ff
Akari Mizunashi <foxnewsnetwork@gmail.com>
2011-11-29 11:54:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nakedads::Application.config.secret_token = '9e781a8d360e3670cd73618cfdc47a12eab59777d708638f032b6a16fc52cd05b3ee6f9320d88365fafba9c626fc325a3f8e7a250e6f4b2da57238da443ea7af'
